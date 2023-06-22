c4e7ec4509f37835945881c356849d987649fb5c
Abdur Rahim <108284077+abdurrahim373@users.noreply.github.com>
2023-06-22 13:25:11 +0600

    adding dist subtree

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './style.css'
-import { createPinia } from 'pinia'
-
-const pinia = createPinia()
-const app = createApp(App)
-app.use(pinia)
-app.mount('#app')
