f53ad6f99a57aaa1a748cf49b8acce3570757a66
Abdur Rahim <108284077+abdurrahim373@users.noreply.github.com>
2023-06-22 16:30:02 +0600

    PWA added

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
 import { createPinia } from 'pinia'
+import './registerServiceWorker'
 
 const pinia = createPinia()
 const app = createApp(App)
